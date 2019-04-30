8d4dfe3771a57a8c47e6a80c57c16ebbdf58be10
Driijko <driijko@gmail.com>
2019-04-30 17:15:35 -0400

    slight modification

--- vowelsAndConsonants.js
+++ vowelsAndConsonants.js
@@ -1,11 +1,14 @@
+// Developed for HackerRank's 10 Days of Javascript challenge
+// A function which first prints of the individual vowels, and then the individual consonants of an input
+// string.
 function vowelsAndConsonants(s) {
     let vowels = ["a", "e", "i", "o", "u"];
     let consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
         "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-    for (char of s) {
+    for (let char of s) {
         if(vowels.includes(char)) console.log(char);
     }
-    for (char of s) {
+    for (let char of s) {
         if(consonants.includes(char)) console.log(char);
     }
 }
